055fd6074890a6e2e2046ea05a9f4634de2dea72
datto <datto@LAPTOP-PFU8NVMG>
2020-11-03 17:07:34 +0100

    Initial commit

---
+++ src/main/java/it/unical/ingsw2020/EsercitazioneNumeriPrimi/App.java
@@ -0,0 +1,14 @@
+package it.unical.ingsw2020.EsercitazioneNumeriPrimi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	MyMath mm = new MyMath();
+        System.out.println( mm.primeNumbersGeneretor(30) );
+    }
+}
