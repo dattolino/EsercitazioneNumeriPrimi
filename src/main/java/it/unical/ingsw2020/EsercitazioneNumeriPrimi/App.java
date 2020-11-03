5fa18be761cf31076f8a33836513ed6c94e4ffb7
datto <datto@LAPTOP-PFU8NVMG>
2020-11-03 17:38:52 +0100

    Second commit

--- src/main/java/it/unical/ingsw2020/EsercitazioneNumeriPrimi/App.java
+++ src/main/java/it/unical/ingsw2020/EsercitazioneNumeriPrimi/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	System.out.println("Modifica");
     	MyMath mm = new MyMath();
         System.out.println( mm.primeNumbersGeneretor(30) );
     }
